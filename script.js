8d18d7777ff0d01a93492e430d6ea2170ad45ae5
unknown <287915124+chopyoz1207-design@users.noreply.github.com>
2026-08-11 21:16:08 +0900

    로그인 오류 수정 시도 한 번 더 레츠

--- script.js
+++ script.js
@@ -10,7 +10,6 @@
    ========================================================= */
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,7 +24,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 let auth = null;
 let db = null;
